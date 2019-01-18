eecc3f857f5e4c91d7dd7b520e05c69489d5943f
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2019-01-18 10:13:02 +0100

    Web server basics
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,29 @@
+use std::io::prelude::{ Read, Write };
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 pub fn serve(interface: String, port: u16) {
+    let listener = TcpListener::bind(format!("{}:{}", interface, port)).unwrap();
+
     println!("Serving to {} at port {}", interface, port);
+    for stream in listener.incoming() {
+        let stream = match stream {
+            Ok(s) => {s},
+            Err(_) => {continue},
+        };
+        println!("New connection from {}", stream.peer_addr().unwrap());
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 40960];
+
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", "hello world!");
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
