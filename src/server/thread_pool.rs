193a552d938b09341a0f8318cbd08ca9ee60f3eb
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2019-01-21 17:19:34 +0100

    Graceful shutdown
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- src/server/thread_pool.rs
+++ src/server/thread_pool.rs
@@ -5,7 +5,7 @@ use std::sync::Mutex;
 
 pub struct ThreadPool {
     threads: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 trait FnBox {
@@ -20,6 +20,11 @@ impl<F: FnOnce()> FnBox for F {
 
 type Job = Box<FnBox + Send>;
 
+enum Message {
+    Job(Job),
+    Terminate,
+}
+
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         let (sender, receiver) = mpsc::channel();
@@ -31,25 +36,44 @@ impl ThreadPool {
     pub fn execute<F>(&self, f: F)
         where F: FnOnce() + Send + 'static
     {
-        self.sender.send(Box::new(f)).unwrap();
+        self.sender.send(Message::Job(Box::new(f))).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        (0..self.threads.len()).for_each(|_| {
+            self.sender.send(Message::Terminate).unwrap();
+        });
+        for thread in &mut self.threads {
+            if let Some(thread) = thread.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, job_rx: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, job_rx: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         Worker {
-            thread: thread::Builder::new().stack_size(100*1024*1024).spawn(move|| {
+            thread: Some(thread::Builder::new().stack_size(100*1024*1024).spawn(move|| {
                 println!("Thread {} started", id);
                 loop {
-                    let f = job_rx.lock().unwrap().recv().unwrap();
-                    println!("Thread {} received job", id);
-                    f.call();
+                    let msg = job_rx.lock().unwrap().recv().unwrap();
+                    match msg {
+                        Message::Job(job) => {
+                            println!("Thread {} received job", id);
+                            job.call();
+                        },
+                        Message::Terminate => break,
+                    }
                 }
-            }).unwrap()
+                println!("Thread {} exiting", id);
+            }).unwrap())
         }
     }
 }
